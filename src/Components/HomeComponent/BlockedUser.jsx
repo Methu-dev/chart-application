2dea40fede2b5850d88c101eeba6ab906d077984
Methu-Dev <engmethu@gmail.com>
2025-04-28 04:00:13 +0600

    added unblock

--- src/Components/HomeComponent/BlockedUser.jsx
+++ src/Components/HomeComponent/BlockedUser.jsx
@@ -36,6 +36,16 @@ function BlockedUser() {
     };
   }, []);
 
+  // unblock dada
+  const handleUnblock = (blockItem) => {
+    const blockRef = ref(db, `blocklist/${blockItem.blockKey}`);
+    remove(blockRef)
+      .catch((error) => {
+        console.error("Unblock Error:", error);
+      });
+  };
+  
+
   return (
     <div>
       {/* Grouplist */}
@@ -78,9 +88,10 @@ function BlockedUser() {
             </div>
             <button
               type="button"
-              class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 cursor-pointer"
+              onClick={() => handleUnblock(blockUser)}
+              className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 cursor-pointer"
             >
-              unblock
+              Unblock
             </button>
           </div>
         ))}
